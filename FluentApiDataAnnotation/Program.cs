84eefef10f5d95f59d44824269de076f13ec00d4
Muradov2004 <ami65143@gmail.com>
2023-06-17 14:57:48 +0400

    Add project files.

---
+++ FluentApiDataAnnotation/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+
+class AcademyContext : DbContext
+{
+    public AcademyContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("");
+    }
+}
