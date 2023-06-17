819808093b2df8a1dc43cf9240bb68de0054a69a
Muradov2004 <ami65143@gmail.com>
2023-06-17 22:09:05 +0400

    Updated

--- FluentApiDataAnnotation/Chair.cs
+++ FluentApiDataAnnotation/Chair.cs
@@ -1,6 +1,8 @@
 ﻿public class Chair
 {
+    public int Id { get; set; }
+    public decimal Financing { get; set; }
+    public string? Name { get; set; }
 
-
-
+    public override string ToString() => $"{Id}. {Name} - {Financing}";
 }
--- FluentApiDataAnnotation/Faculty.cs
+++ FluentApiDataAnnotation/Faculty.cs
@@ -1,6 +1,9 @@
-﻿
+﻿using Microsoft.VisualBasic;
 
 public class Faculty
 {
-    
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public override string ToString() => $"{Id}. {Name}";
 }
