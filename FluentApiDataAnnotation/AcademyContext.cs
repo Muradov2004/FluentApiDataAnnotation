9dd3f06a1c308c35e204757f0c1fa1fa9daa6aa5
Muradov2004 <ami65143@gmail.com>
2023-06-17 23:25:31 +0400

    Updated

--- FluentApiDataAnnotation/AcademyContext.cs
+++ FluentApiDataAnnotation/AcademyContext.cs
@@ -3,6 +3,11 @@ using Microsoft.Extensions.Configuration;
 
 public class AcademyContext : DbContext
 {
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<Chair> Chairs { get; set; }
+    public DbSet<Faculty> Faculties { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+
     public AcademyContext()
     {
         Database.EnsureDeleted();
@@ -22,6 +27,53 @@ public class AcademyContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //modelBuilder.Entity<>()
+
+        modelBuilder.Entity<Group>(entity =>
+        {
+            entity.Property(e => e.Id)
+                                    .ValueGeneratedOnAdd();
+            entity.Property(e => e.GroupName)
+                                            .HasMaxLength(10)
+                                            .IsRequired();
+            entity.HasIndex(e => e.GroupName)
+                                            .IsUnique();
+            entity.Property(e => e.Rating)
+                                        .IsRequired()
+                                        .HasAnnotation("MinValue", 0)
+                                        .HasAnnotation("MaxValue", 5);
+            entity.Property(e => e.Year)
+                                        .IsRequired()
+                                        .HasAnnotation("MinValue", 1)
+                                        .HasAnnotation("MaxValue", 5);
+        });
+
+        modelBuilder.Entity<Chair>(entity =>
+        {
+            entity.Property(e => e.Id)
+                                    .ValueGeneratedOnAdd();
+            entity.Property(e => e.Name)
+                                        .HasMaxLength(100)
+                                        .IsRequired();
+            entity.HasIndex(e => e.Name)
+                                        .IsUnique();
+            entity.Property(e => e.Financing)
+                                        .IsRequired()
+                                        .HasColumnType("money")
+                                        .HasAnnotation("MinValue", 0)
+                                        .HasDefaultValue(0);
+        });
+
+        modelBuilder.Entity<Faculty>(entity =>
+        {
+            entity.Property(e => e.Id)
+                                    .ValueGeneratedOnAdd();
+            entity.Property(e => e.Name)
+                                        .HasMaxLength(100)
+                                        .IsRequired();
+            entity.HasIndex(e => e.Name)
+                                        .IsUnique();
+        });
+
+
     }
 }
--- FluentApiDataAnnotation/Program.cs
+++ FluentApiDataAnnotation/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using (AcademyContext database = new())
+{
+    Group group = new()
+    {
+        GroupName = "group1",
+        Year = 1,
+        Rating = 1
+    };
+
+    Chair chair = new()
+    {
+        Name = "chair1",
+        Financing = 10
+    };
+
+    Faculty faculty = new()
+    {
+        Name = "faculty"
+    };
+
+    Teacher teacher = new()
+    {
+        Name = "Salam",
+        Surname = "Salammm",
+        Premium = 100,
+        Salary = 100,
+        EmploymentDate = DateTime.Now,
+    };
+
+    database.Groups.Add(group);
+    database.Chairs.Add(chair);
+    database.Teachers.Add(teacher);
+    database.SaveChanges();
+}
